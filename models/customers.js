79209de5d501d03eb3297c93a3f427b97792732d
Anqi Li <425765498@qq.com>
2018-10-04 23:09:26 +0100

    Customers Built

--- models/customers.js
+++ models/customers.js
@@ -1,5 +1,5 @@
 const cosmetics = [
-    { id: 3000, name : 'Lily', account : 'Maybelline', publisher : 2000},
-    { id: 3001, name : 'Joe', account : 'Maybelline', publisher : 2000},
-    { id: 3002, name : 'Christina', account : 'Ponds', publisher : 2001}
+    { id: 3000, name : 'Lily', account : '123456@gmail.com', phoneNum : 123456, address: ""},
+    { id: 3001, name : 'Joe', account : '123456@163.com', phoneNum : 123456, address: ""},
+    { id: 3002, name : 'Christina', account : '123456@qq.com', phoneNum : 123456, address: ""}
 ]
